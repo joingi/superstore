060f2f1977b4cd26b4171159e9c8fbab5b2b75a4
Johann I <joi.ingi@me.com>
2017-05-08 22:42:34 +0200

    finished desktop and mobile menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -22,11 +22,17 @@ gulp.task('watch', function() {
     browserSync.reload();
   });
 
+    gulp.watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
    return gulp.src('/app/temp/styles/styles.css')
     .pipe(browserSync.stream());
-
 });
 
+gulp.task('scriptsRefresh', ['scripts'], function() {
+    browserSync.reload();
+});
